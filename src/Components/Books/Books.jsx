474055725395aea0c0cb40183b8b8ab998b74267
momin096 <momin09609@gmail.com>
2025-01-23 08:36:57 +0600

    books section added

--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import Book from "../Book/Book";
 
 const Books = () => {
+
+    const [books, setBooks] = useState([]);
+    
+    useEffect(()=>{
+        fetch('/public/booksData.json')
+        .then(res => res.json())
+        .then(data => setBooks(data));
+    },[])
+    console.log(books);
+    
+
     return (
         <div>
-            <h2>Books</h2>            
+            <h2 className="text-5xl font-bold text-center">Books</h2>   
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                {
+                    books.map(book => <Book key={book.bookId} book={book}></Book>)
+                }
+            </div>         
         </div>
     );
 };
